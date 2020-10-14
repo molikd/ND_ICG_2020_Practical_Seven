d2c8672c3dd7fee46e5cdfe313c2610478db0322
Mae <merimaat1@gmail.com>
2020-10-14 05:13:47 -0400

    Updated usage comments

--- practical.sh
+++ practical.sh
@@ -13,4 +13,4 @@ julia ./sequenceDiversity.jl practical_four.fasta
 # instead of using the Julia REPL
 #tar -zxvf GeneticVariation.jl-0.4.0.tar.gz
 #cd GeneticVariation.jl-0.4.0
-#julia ./sequenceDiversity.jl ../practical_four.fasta
+#julia ./../sequenceDiversity.jl ../practical_four.fasta
